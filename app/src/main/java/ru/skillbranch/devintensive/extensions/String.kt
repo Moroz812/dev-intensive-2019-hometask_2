4d332923952f2e4321474196acbd1a3e136c54a9
imfailing <androsov.sv@gmail.com>
2019-07-10 23:17:44 +0300

    fixes

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -1,13 +1,10 @@
 package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(size: Int = 16): String {
-    val result = this.trim()
-    if (result.length <= size)
-        return result
-    return result.substring(0, size).trimEnd() + "..."
+    val resStr = this.trim()
+    if (resStr.length <= size)
+        return resStr
+    return resStr.substring(0, size).trimEnd() + "..."
 }
 
-fun String.stripHtml() = this
-        .replace(Regex("<[^>]*>"), "")
-        .replace(Regex("&amp;|&lt;|&gt;|&quot;|&apos;|&#\\d+;"), "")
-        .replace(Regex(" +"), " ")
+fun String.stripHtml() = this.replace(Regex("<[^>]*>"), "").replace(Regex("&amp;|&lt;|&gt;|&quot;|&apos;|&#\\d+;"), "").replace(Regex(" +"), " ")
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,12 +1,9 @@
 package ru.skillbranch.devintensive.utils
 
-object tils {
+object Utils {
 
-    private val translitMap = mapOf(
-        'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z",
-        'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r",
-        'с' to "s", 'т' to "t", 'у' to "u", 'ф' to "f", 'х' to "h", 'ц' to "c", 'ч' to "ch", 'ш' to "sh", 'щ' to "sh",
-        'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
+    private val translitMapping = mapOf(
+        'а' to "a", 'б' to "b", 'в' to "v", 'г' to "g", 'д' to "d", 'е' to "e", 'ё' to "e", 'ж' to "zh", 'з' to "z",'и' to "i", 'й' to "i", 'к' to "k", 'л' to "l", 'м' to "m", 'н' to "n", 'о' to "o", 'п' to "p", 'р' to "r",'ъ' to "", 'ы' to "i", 'ь' to "", 'э' to "e", 'ю' to "yu", 'я' to "ya"
     )
 
     fun parseFullName(fullName: String?): Pair<String?, String?> {
@@ -28,8 +25,8 @@ object tils {
         payload.forEach {
             result += when {
                 it == ' ' -> divider
-                it.isUpperCase() -> translitMap[it.toLowerCase()]?.capitalize() ?: it.toString()
-                else -> translitMap[it] ?: it.toString()
+                it.isUpperCase() -> translitMapping[it.toLowerCase()]?.capitalize() ?: it.toString()
+                else -> translitMapping[it] ?: it.toString()
             }
         }
         return result
